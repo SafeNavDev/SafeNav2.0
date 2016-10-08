e28fa1c77cc1804d46ec1530d03ca08ea34cec37
Jonathan Butturini <jakmb@umich.edu>
2016-10-08 17:43:33 -0400

    finalized fetching crime data

--- FetchCrimeData.py
+++ FetchCrimeData.py
@@ -1,5 +1,4 @@
 import requests
-import math
 
 
 class FetchCrimeData:
@@ -11,24 +10,28 @@ class FetchCrimeData:
             return str(month)
 
     # NOTE: Requires location list formatted: ['longitude', 'latitude']
-    def query_database(year_in, month_in, day_in, category_in, location):
+    def query_database(year_in, month_in, day_in, category_in, location, radius):
+        incidents = []
         # This generates the category of crime
         category_param = 'category=' + category_in
 
-        # This generates the date parameter
+        # This generates the date paadfsasdframeter
         month = FetchCrimeData.format_month(month_in)
         date = year_in + '-' + month + '-' + day_in + 'T12:00:00'
         prev_month = FetchCrimeData.format_month(int(month) - 2)
         prev_date = year_in + '-' + prev_month + '-' + day_in + 'T12:00:00'
-        date_param = '&$where=incidentdate between ' + "'" + prev_date + "'" + ' and ' + "'" + date + "'"
+        date_param = '&$where= incidentdate between ' + "'" + prev_date + "'" + ' and ' + "'" + date + "'"
 
         # This generates the radius in which we are searching
-        location_param = '&$where=within_circle(location, ' + location[0] + ', ' + location[1] + ', 500)'
+        location_param = 'and within_circle(location, ' + location[0] + ', ' + location[1] + ', ' + str(radius) + ')'
         print(location_param)
 
         # Finally, concatenate and pass http get request
-        print('https://data.detroitmi.gov/resource/i9ph-uyrp.json?' + category_param
-            + date_param + location_param)
+        url = 'https://data.detroitmi.gov/resource/i9ph-uyrp.json?' + category_param + date_param + location_param
+        data = requests.get(url).json()
+        for x in data:
+            incidents.append(x['location']['coordinates'])
+        return incidents
 
-data = FetchCrimeData.query_database('2016', '10', '08', 'ARSON', ['42.3417707', '-83.0601714'])
-print(data.json())
+data = FetchCrimeData.query_database('2016', '10', '08', 'ARSON', ['42.3417707', '-83.0601714'], 3000)
+print(data)
