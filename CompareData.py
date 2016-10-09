8d8ac836c5044fb90557a1a3777a7998a911eaa6
Jonathan Butturini <jakmb@umich.edu>
2016-10-08 22:26:27 -0400

    returns set of points along route that are crimes

--- CompareData.py
+++ CompareData.py
@@ -2,20 +2,19 @@ import requests
 from FetchCrimeData import FetchCrimeData
 from FilteredPath import FilteredPath
 
-segment_length_miles = .025
-segment_length_meters = 500
+segment_length_miles = .15
+segment_length_meters = 200
 
 class CompareData:
-    def return_conflicts(lat1_in, long1_in, lat2_in, long2_in, year_in, month_in, day_in, category_in):
+    def return_conflicts(lat1_in, long1_in, lat2_in, long2_in, year_in, month_in, day_in):
         incidents_on_route = []
         data = FilteredPath.get_data(lat1_in, long1_in, lat2_in, long2_in)
         path = FilteredPath.filter_path_theory(data, segment_length_miles)
-        CrossReferencer = FetchCrimeData(year_in, month_in, day_in, category_in)
+        CrossReferencer = FetchCrimeData(year_in, month_in, day_in)
         for x in path:
             print(x)
-            incidents_on_route.append(CrossReferencer.static_query(x,segment_length_meters))
-        print(incidents_on_route)
+            CrossReferencer.static_query(incidents_on_route, x, segment_length_meters)
         return incidents_on_route
 
-all_incidents = CompareData.return_conflicts('42.3417707', '-83.0601714', '42.377', '-83.2089', '2015', '10', '08', 'BURGLARY')
+all_incidents = CompareData.return_conflicts('42.3418', '-83.0602', '42.3591', '-83.0665', '2015', '10', '08')
 print(all_incidents)
--- FetchCrimeData.py
+++ FetchCrimeData.py
@@ -6,17 +6,7 @@ class FetchCrimeData:
     static_category_param = ""
     static_date_param = ""
 
-    # A custom initializer for efficiency when computing multiple lines along a path
-    # Having certain data members stored is more efficient
-    def __init__(self, year_in, month_in, day_in):
-        month_in = FetchCrimeData.format_month(int(month_in))
-        date = year_in + '-' + month_in + '-' + day_in + 'T12:00:00'
-        prev_month = FetchCrimeData.format_month(int(month_in) - 2)
-        prev_date = year_in + '-' + prev_month + '-' + day_in + 'T12:00:00'
-        self.static_category_param = 'category=' + 'ASSAULT' + 
-        self.static_date_param = '&$where= incidentdate between ' + "'" + prev_date + "'" + ' and ' + "'" + date + "'"
-
-
+    # A helper function to format the month field
     def format_month(month):
         int_month = int(month)
         if int_month < 10:
@@ -24,44 +14,47 @@ class FetchCrimeData:
         else:
             return str(month)
 
+    # A custom initializer for efficiency when computing multiple lines along a path
+    # Having certain data members stored is more efficient
+    def __init__(self, year_in, month_in, day_in):
+        month_in = FetchCrimeData.format_month(int(month_in))
+        date = year_in + '-' + month_in + '-' + day_in + 'T12:00:00'
+        prev_month = FetchCrimeData.format_month(int(month_in) - 2)
+        prev_date = year_in + '-' + prev_month + '-' + day_in + 'T12:00:00'
+        self.static_category_param = """and(category='ASSAULT' or category='ROBBERY' or category='AGGRAVATED ASSAULT' or category='HOMICIDE' or category='KIDNAPPING' or category='DRUNKENNESS' or category='DISORDERLY CONDUCT' or category='DANGEROUS DRUGS')"""
+        self.static_date_param = '$where= (incidentdate between ' + "'" + prev_date + "'" + ' and ' + "'" + date + "'"
+
+    # NOTE: DEPRECATED! Use initialized and static_query
     # NOTE: Requires location list formatted: ['longitude', 'latitude']
     def query_database(year_in, month_in, day_in, category_in, location, radius):
         incidents = []
         # This generates the category of crime
-        category_param = 'category=' + category_in
+        category_param = 'and (category=' + category_in +')'
 
         # This generates the date paadfsasdframeter
         month = FetchCrimeData.format_month(month_in)
         date = year_in + '-' + month + '-' + day_in + 'T12:00:00'
         prev_month = FetchCrimeData.format_month(int(month) - 2)
         prev_date = year_in + '-' + prev_month + '-' + day_in + 'T12:00:00'
-        date_param = '&$where= incidentdate between ' + "'" + prev_date + "'" + ' and ' + "'" + date + "'"
+        date_param = '$where= (incidentdate between ' + "'" + prev_date + "'" + ' and ' + "'" + date + "'"
 
         # This generates the radius in which we are searching
-        location_param = 'and within_circle(location, ' + location[0] + ', ' + location[1] + ', ' + str(radius) + ')'
+        location_param = 'and within_circle(location, ' + location[1] + ', ' + location[0] + ', ' + str(radius) + '))'
 
         # Finally, concatenate and pass http get request
-        url = 'https://data.detroitmi.gov/resource/i9ph-uyrp.json?' + category_param + date_param + location_param
+        url = 'https://data.detroitmi.gov/resource/i9ph-uyrp.json?' + date_param + location_param + category_param
         local_data = requests.get(url).json()
         for x in local_data:
             incidents.append(x['location']['coordinates'])
         return incidents
 
     # Allows a more efficient query by not initializing local variables every time
-    def static_query(self, location, radius):
-        incidents = []
-        location_param = 'and within_circle(location, ' + str(location[0]) + ', ' + str(location[1]) + ', ' + str(radius) + ')'
-        url = self.static_base_url + self.static_category_param + self.static_date_param + location_param
-        print(url)
+    def static_query(self, incidents, location, radius):
+        location_param = 'and within_circle(location, ' + str(location[1]) + ', ' + str(location[0]) + ', ' + str(radius) + '))'
+        url = self.static_base_url + self.static_date_param + location_param + self.static_category_param
         local_data = requests.get(url).json()
-        for x in local_data:
-            incidents.append(x['location']['coordinates'])
-        if not incidents:
-            print("EMPTY")
+        if local_data:
+            for x in local_data:
+                incidents.append(x['location']['coordinates'])
         else:
-            print(incidents)
-        return incidents
-
-DataObject = FetchCrimeData('2015', '10', '08', 'BURGLARY')
-data = DataObject.static_query(['42.37742', '-83.20891'], 3000)
-print(data)
+            return incidents
